f6e746ca04442e590f1ff45bd82ae27c88bbb8b6
hochan <hochan0115@gmail.com>
2023-10-10 20:10:12 +0900

    feat: Imported kizitonwose calendar library

---
+++ frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyView.kt
@@ -0,0 +1,12 @@
+package com.example.calendy.view.monthlyview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calendy.R
+
+class MonthlyView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monthly_view)
+    }
+}
