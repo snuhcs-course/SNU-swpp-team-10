2458ca2a4ecac51a2149ad0aca362b6ecd0fd846
hochan <hochan0115@gmail.com>
2023-10-15 15:41:43 +0900

    feat: Added MonthlyView.java with layout files and changed material to 1.8.0 from 1.10.0

---
+++ frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyView.java
@@ -0,0 +1,17 @@
+package com.example.calendy.view.monthlyview;
+
+import androidx.activity.ComponentActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calendy.R;
+
+public class MonthlyView extends ComponentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_view);
+    }
+}
