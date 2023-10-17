465c65b1c574711ac9106d9088617df630435990
HHCHU <ded06031@snu.ac.kr>
2023-10-17 11:02:17 +0900

    feat: Change Month and Week to Korean

--- frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyView.java
+++ frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyView.java
@@ -8,12 +8,23 @@ import android.os.Bundle;
 import com.example.calendy.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
 
 public class MonthlyView extends ComponentActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+    private MaterialCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_view);
+
+        calendarView = findViewById(R.id.materialMonthlyView);
+
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+
     }
 }
