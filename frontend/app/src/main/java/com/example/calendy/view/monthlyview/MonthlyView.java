8ad445d9fb70650828b1a3a3a0bafd6f138eb4ac
hochan <hochan0115@gmail.com>
2023-10-17 12:00:37 +0900

    feat: Added decorators to sunday, saturday, today

--- frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyView.java
+++ frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyView.java
@@ -1,19 +1,46 @@
 package com.example.calendy.view.monthlyview;
 
 import androidx.activity.ComponentActivity;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.os.Bundle;
 
 import com.example.calendy.R;
+import com.example.calendy.view.monthlyview.decorator.OneDayDecorator;
+import com.example.calendy.view.monthlyview.decorator.SaturdayDecorator;
+import com.example.calendy.view.monthlyview.decorator.SundayDecorator;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class MonthlyView extends ComponentActivity {
 
+    MaterialCalendarView materialCalendarView;
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_view);
+        materialCalendarView = findViewById(R.id.materialMonthlyView);
+
+
+
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2017, 0, 1))
+                .setMaximumDate(CalendarDay.from(2030, 11, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                oneDayDecorator);
     }
+
+
 }
---
+++ frontend/app/src/main/java/com/example/calendy/view/monthlyview/decorator/OneDayDecorator.java
@@ -0,0 +1,43 @@
+package com.example.calendy.view.monthlyview.decorator;
+
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+import java.util.Date;
+
+public class OneDayDecorator implements DayViewDecorator {
+
+    private CalendarDay date;
+
+    public OneDayDecorator() {
+        date = CalendarDay.today();
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return date != null && day.equals(date);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new StyleSpan(Typeface.BOLD));
+        view.addSpan(new RelativeSizeSpan(1.4f));
+        view.addSpan(new ForegroundColorSpan(Color.GREEN));
+    }
+
+    /**
+     * We're changing the internals, so make sure to call {@linkplain \MaterialCalendarView#invalidateDecorators()}
+     */
+    public void setDate(Date date) {
+        this.date = CalendarDay.from(date);
+    }
+
+}
