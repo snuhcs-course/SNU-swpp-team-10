0c9875946dead575c8c24dab5adf8905f355cf61
kimkun07 <kimkun07@naver.com>
2023-10-20 18:48:48 +0900

    Fix: Category default id should be 0

--- frontend/app/src/main/java/com/example/calendy/data/category/Category.kt
+++ frontend/app/src/main/java/com/example/calendy/data/category/Category.kt
@@ -2,20 +2,19 @@ package com.example.calendy.data.category
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "category")
 data class Category(
-        @PrimaryKey(autoGenerate = true)
-        @ColumnInfo(name = "id")
-        @SerializedName("id")
-        var id: Int = 1,
-        @ColumnInfo(name = "title")
-        @SerializedName("title")
-        var title: String,
-        @ColumnInfo(name = "default_priority")
-        @SerializedName("default_priority")
-        var defaultPriority: Int
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    @SerializedName("id")
+    var id: Int = 0,
+    @ColumnInfo(name = "title")
+    @SerializedName("title")
+    var title: String,
+    @ColumnInfo(name = "default_priority")
+    @SerializedName("default_priority")
+    var defaultPriority: Int
 )
