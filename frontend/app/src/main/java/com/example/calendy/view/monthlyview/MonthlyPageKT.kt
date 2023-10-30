495aa4b3c12b816f6cbd797d2e54dd7741f87a44
kimkun07 <kimkun07@naver.com>
2023-10-30 22:26:50 +0900

    FIx: dot decorator removed 실시간으로

--- frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyPageKT.kt
+++ frontend/app/src/main/java/com/example/calendy/view/monthlyview/MonthlyPageKT.kt
@@ -3,7 +3,7 @@ package com.example.calendy.view.monthlyview
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,116 +36,138 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
 import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter
+import kotlinx.coroutines.delay
 import java.util.Calendar
 import java.util.Hashtable
 
 @Composable
-fun MonthlyPageKT (
+fun MonthlyPageKT(
     monthlyViewModel: MonthlyViewModel = viewModel(factory = AppViewModelProvider.Factory),
     onNavigateToEditPage: (id: Int?, type: Plan.PlanType) -> Unit
-)
-{
+) {
 
     val custom_months = stringArrayResource(id = R.array.custom_months)
     val custom_weekdays = stringArrayResource(id = R.array.custom_weekdays)
 
-    var selectedDate:CalendarDay by remember{ mutableStateOf(CalendarDay.today()) }
-    var selectedDayDecorator :SelectedDayDecorator
-    var planOfMonth: Hashtable<CalendarDay, MutableList<Plan>> = Hashtable( )
+    var selectedDate: CalendarDay by remember { mutableStateOf(CalendarDay.today()) }
+    var selectedDayDecorator: SelectedDayDecorator
+    var planOfMonth: Hashtable<CalendarDay, MutableList<Plan>> = Hashtable()
     var dotDecorator: DotDecorator? = null
     val oneDayDecorator = OneDayDecorator()
 
 //    val planList: List<Plan> by monthlyViewModel.planList.collectAsState()
-    val planList: List<Plan> by monthlyViewModel.getPlanListState(CalendarDay.from(2023,0,20),CalendarDay.from(2023,11,20)).collectAsState()
-
-    var openListPopup by remember{mutableStateOf(false)}
-
-
-    fun openDayPlanListPopup(selectedDate:CalendarDay) { openListPopup=true }
-    fun openAddPlanPopup(selectedDate:CalendarDay){}
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = {
-            context->
-            val calendar = MaterialCalendarView(context)
-            calendar.apply {
-
-                // initial setting for calendar view
-                setTitleFormatter(MonthArrayTitleFormatter(custom_months))
-                setWeekDayFormatter(ArrayWeekDayFormatter(custom_weekdays))
-                setSelectedDate(selectedDate)
-                showOtherDates=MaterialCalendarView.SHOW_OTHER_MONTHS
-                state().edit()
-                    .setFirstDayOfWeek(Calendar.SUNDAY)
-                    .setMinimumDate(CalendarDay.from(2000, 0, 1))   //from 2000.1.1
-                    .setMaximumDate(CalendarDay.from(2030, 11, 31)) //to 2030.12.31
-                    .setCalendarDisplayMode(CalendarMode.MONTHS)
-                    .commit();
-
-                // selected day decorator initialization
-                selectedDayDecorator = SelectedDayDecorator(CalendarDay.today(), context)
-
-                //add dot decorator
-                addDecorators(
-                    SundayDecorator(),
-                    SaturdayDecorator(),
-                    oneDayDecorator,
-                    selectedDayDecorator,
-                )
-
-                setOnDateChangedListener(OnDateSelectedListener { widget, date, selected ->
-
-                    // selected date changed
-                    if (selectedDate == date )
-                        if(planOfMonth!!.containsKey(date)) openDayPlanListPopup(selectedDate)
-                        else openAddPlanPopup(selectedDate)
-                    removeDecorator(selectedDayDecorator)
-                    selectedDate = date
-                    selectedDayDecorator = SelectedDayDecorator(selectedDate, context)
-                    addDecorators(selectedDayDecorator)
-                })
-                // event
-                setOnMonthChangedListener(
-                    // selected month changed
-                    OnMonthChangedListener { widget, date ->
-                        //TODO: change planList
-                    })
-
+//    val planList: List<Plan> by monthlyViewModel.getPlanListState(CalendarDay.from(2023,0,20),CalendarDay.from(2023,11,20)).collectAsState()
+    var planList: List<Plan> by remember {
+        mutableStateOf(emptyList())
+    }
+    LaunchedEffect(key1 = planList, block = {
+        while (true) {
+            monthlyViewModel.getPlanListState(
+                CalendarDay.from(2023, 0, 20),
+                CalendarDay.from(2023, 11, 20)
+            ).collect {
+                Log.d("GUN", it.toString())
+                if (it.isNotEmpty()) {
+                    planList = it
+                }
             }
-        },
-        update = {
-            view->
+
+            delay(5000)
+        }
+    })
+
+    var openListPopup by remember { mutableStateOf(false) }
+
+
+    fun openDayPlanListPopup(selectedDate: CalendarDay) {
+        openListPopup = true
+    }
+
+    fun openAddPlanPopup(selectedDate: CalendarDay) {}
+    AndroidView(modifier = Modifier.fillMaxSize(), factory = { context ->
+        val calendar = MaterialCalendarView(context)
+        calendar.apply {
+
+            // initial setting for calendar view
+            setTitleFormatter(MonthArrayTitleFormatter(custom_months))
+            setWeekDayFormatter(ArrayWeekDayFormatter(custom_weekdays))
+            setSelectedDate(selectedDate)
+            showOtherDates = MaterialCalendarView.SHOW_OTHER_MONTHS
+            state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2000, 0, 1))   //from 2000.1.1
+                .setMaximumDate(CalendarDay.from(2030, 11, 31)) //to 2030.12.31
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+            // selected day decorator initialization
+            selectedDayDecorator = SelectedDayDecorator(CalendarDay.today(), context)
+
+            //add dot decorator
+            addDecorators(
+                SundayDecorator(),
+                SaturdayDecorator(),
+                oneDayDecorator,
+                selectedDayDecorator,
+            )
+
+            setOnDateChangedListener(OnDateSelectedListener { widget, date, selected ->
+
+                // selected date changed
+                if (selectedDate==date) if (planOfMonth!!.containsKey(date)) openDayPlanListPopup(
+                    selectedDate
+                )
+                else openAddPlanPopup(selectedDate)
+                removeDecorator(selectedDayDecorator)
+                selectedDate = date
+                selectedDayDecorator = SelectedDayDecorator(selectedDate, context)
+                addDecorators(selectedDayDecorator)
+            })
+            // event
+            setOnMonthChangedListener(
+                // selected month changed
+                OnMonthChangedListener { widget, date ->
+                    //TODO: change planList
+                })
+
+        }
+    }, update = { view ->
 //            planOfMonth= planListToHash(planList)
 //            dotDecorator = DotDecorator(planOfMonth)
-            Log.d("hochan","updated")
+        Log.d("hochan", "updated")
 
-            planOfMonth = planListToHash(planList)
-            view.removeDecorator(dotDecorator)
-            dotDecorator = DotDecorator(planOfMonth)
-            view.addDecorators(dotDecorator)
-        }
-    )
+        planOfMonth = planListToHash(planList)
+        Log.d("GUN", "UPDATE: " + planOfMonth.toString())
+        dotDecorator = DotDecorator(planOfMonth)
+        view.removeDecorators()
+        view.addDecorators(
+            SundayDecorator(),
+            SaturdayDecorator(),
+            oneDayDecorator,
+            dotDecorator
+        )
+    })
 
-    if(openListPopup){
-        MonthlyDayPlanListPopupKT(onDismissRequest = {openListPopup=false}, selectedDate = selectedDate,
-                                  onNavigateToEditPage = onNavigateToEditPage)
+    if (openListPopup) {
+        MonthlyDayPlanListPopupKT(
+            onDismissRequest = { openListPopup = false },
+            selectedDate = selectedDate,
+            onNavigateToEditPage = onNavigateToEditPage
+        )
     }
 
 
 }
 
 
-
-fun planListToHash(planList:List<Plan>):Hashtable<CalendarDay,MutableList<Plan>>{
-    val planOfMonth :Hashtable<CalendarDay,MutableList<Plan>> = Hashtable()
-    for(p in planList){
-        val day:CalendarDay =
-            when(p){
-                is Schedule -> p.startTime.toCalendarDay()
-                is Todo -> p.dueTime.toCalendarDay()
-            }
-        if(!planOfMonth!!.containsKey(day))
-            planOfMonth[day]= mutableListOf()
+fun planListToHash(planList: List<Plan>): Hashtable<CalendarDay, MutableList<Plan>> {
+    val planOfMonth: Hashtable<CalendarDay, MutableList<Plan>> = Hashtable()
+    for (p in planList) {
+        val day: CalendarDay = when (p) {
+            is Schedule -> p.startTime.toCalendarDay()
+            is Todo     -> p.dueTime.toCalendarDay()
+        }
+        if (!planOfMonth!!.containsKey(day)) planOfMonth[day] = mutableListOf()
 
         val list = planOfMonth[day]
         list!!.add(p)
@@ -153,7 +175,7 @@ fun planListToHash(planList:List<Plan>):Hashtable<CalendarDay,MutableList<Plan>>
     return planOfMonth
 }
 
-@Preview(showBackground=false, name="Monthly Calendar Preview")
+@Preview(showBackground = false, name = "Monthly Calendar Preview")
 @Composable
 fun MonthlyCalendarPreview() {
 
@@ -164,5 +186,6 @@ fun MonthlyCalendarPreview() {
             todoRepository = DummyTodoRepository(),
             categoryRepository = DummyCategoryRepository(),
             repeatGroupRepository = DummyRepeatGroupRepository(),
-        )) { _, _ -> }
+        )
+    ) { _, _ -> }
 }
