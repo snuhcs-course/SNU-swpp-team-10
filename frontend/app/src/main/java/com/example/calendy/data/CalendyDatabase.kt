94628539a2847f03ae7fcff00f82f854ff90195c
Jaemin Choi <jmchoi1993@snu.ac.kr>
2023-10-30 16:56:28 +0900

    fix: update roomconverter

--- frontend/app/src/main/java/com/example/calendy/data/CalendyDatabase.kt
+++ frontend/app/src/main/java/com/example/calendy/data/CalendyDatabase.kt
@@ -18,7 +18,7 @@ import com.example.calendy.data.repeatgroup.RepeatGroupDao
 
 @Database(
     entities = [Schedule::class, Todo::class, Category::class, Message::class, RepeatGroup::class],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 @TypeConverters(RoomConverters::class)
--- frontend/app/src/main/java/com/example/calendy/data/RoomConverters.kt
+++ frontend/app/src/main/java/com/example/calendy/data/RoomConverters.kt
@@ -1,16 +1,25 @@
 package com.example.calendy.data
 
 import androidx.room.TypeConverter
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 class RoomConverters {
+    private val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
     @TypeConverter
-    fun fromTimestamp(value: Long): Date {
-        return Date(value)
+    fun fromString(value: String?): Date? {
+        return try {
+            value?.let { format.parse(it) }
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            null
+        }
     }
 
     @TypeConverter
-    fun dateToTimestamp(date: Date): Long {
-        return date.time
+    fun dateToString(date: Date?): String? {
+        return date?.let { format.format(it) }
     }
 }
